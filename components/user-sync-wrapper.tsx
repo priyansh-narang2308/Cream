ae030641e192be4b14d625d63389f99ea3bea84e
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-11-19 01:06:07 +0530

    feat: Inside use sync wrapper made the yn user fucntion to sync with the database

--- components/user-sync-wrapper.tsx
+++ components/user-sync-wrapper.tsx
@@ -3,7 +3,7 @@
 import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/nextjs";
 import { useMutation } from "convex/react";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { LoadingSpinner } from "./loading-spinner";
 
 const UserSyncWrapper = ({ children }: { children: React.ReactNode }) => {
@@ -12,7 +12,34 @@ const UserSyncWrapper = ({ children }: { children: React.ReactNode }) => {
   const [error, setError] = useState<string | null>(null);
 
   //Get the user to sync it by mutation
-  const cretaeorUpdateUser = useMutation(api.users.upsertUser);
+  const createOrUpdateUser = useMutation(api.users.upsertUser);
+
+  const syncUser = useCallback(async () => {
+    if (!user?.id) {
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+      setError(null);
+
+      await createOrUpdateUser({
+        userId: user?.id,
+        name:
+          user?.fullName ||
+          user?.firstName ||
+          user?.emailAddresses[0].emailAddress ||
+          "Unknown User",
+        email: user?.emailAddresses[0].emailAddress || "",
+        imageUrl: user?.imageUrl || "",
+      });
+    } catch (error) {
+      console.log("Failed to sync user: ", error);
+      setError(error instanceof Error ? error.message : "Failed to sync user");
+    } finally {
+      setIsLoading(false);
+    }
+  }, [createOrUpdateUser, user]);
 
   if (!isUserLoaded || isLoading) {
     return (
